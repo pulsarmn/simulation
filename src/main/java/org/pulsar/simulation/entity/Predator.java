746f9f101d7825abbe78009498142024393e79ce
Кузичев Александр <pulsarmn@yandex.ru>
2025-08-25 14:23:37 +0300

    add: create the beta version of the `Predator` class

---
+++ src/main/java/org/pulsar/simulation/entity/Predator.java
@@ -0,0 +1,19 @@
+package org.pulsar.simulation.entity;
+
+
+public class Predator extends Creature {
+
+    private final int attackPower;
+
+    private static final String PREDATOR_TEXTURE = "🐺";
+
+    public Predator(Health health, int attackPower) {
+        super(PREDATOR_TEXTURE, health);
+        this.attackPower = attackPower;
+    }
+
+    @Override
+    public void makeMove() {
+        // TODO
+    }
+}
