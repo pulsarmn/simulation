bc03aac6738ae0155344dc844c26f665e4fb4540
Кузичев Александр <pulsarmn@yandex.ru>
2025-08-25 13:59:20 +0300

    add: create the `Rock` class

---
+++ src/main/java/org/pulsar/simulation/entity/Rock.java
@@ -0,0 +1,11 @@
+package org.pulsar.simulation.entity;
+
+
+public class Rock extends Entity {
+
+    private static final String ROCK_TEXTURE = "🗿";
+
+    public Rock() {
+        super(ROCK_TEXTURE);
+    }
+}
