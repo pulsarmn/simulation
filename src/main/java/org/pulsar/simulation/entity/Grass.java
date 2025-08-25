cd8ce10ad1799fd041dae94f36104d2d54658c46
Кузичев Александр <pulsarmn@yandex.ru>
2025-08-25 14:00:22 +0300

    add: create the `Grass` class

---
+++ src/main/java/org/pulsar/simulation/entity/Grass.java
@@ -0,0 +1,11 @@
+package org.pulsar.simulation.entity;
+
+
+public class Grass extends Entity {
+
+    private static final String GRASS_TEXTURE = "🍀";
+
+    public Grass() {
+        super(GRASS_TEXTURE);
+    }
+}
