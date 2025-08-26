90abeee57fdf4f637897aa57ff609cab822f4971
Кузичев Александр <pulsarmn@yandex.ru>
2025-08-26 17:21:20 +0300

    add: create a beta version of the `MapRenderer` class

---
+++ src/main/java/org/pulsar/simulation/render/MapRenderer.java
@@ -0,0 +1,17 @@
+package org.pulsar.simulation.render;
+
+import org.pulsar.simulation.FieldMap;
+
+public class MapRenderer implements Renderer {
+
+    private final FieldMap map;
+
+    public MapRenderer(FieldMap map) {
+        this.map = map;
+    }
+
+    @Override
+    public void render() {
+        // TODO
+    }
+}
