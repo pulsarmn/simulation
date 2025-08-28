f80047f314b0af611cae0b0d231846b0b53022e1
Кузичев Александр <pulsarmn@yandex.ru>
2025-08-28 16:16:16 +0300

    refactor: the `RockGeneratorAction` and the `TreeGeneratorAction` classes

--- src/main/java/org/pulsar/simulation/action/RockGeneratorAction.java
+++ src/main/java/org/pulsar/simulation/action/RockGeneratorAction.java
@@ -1,18 +1,11 @@
 package org.pulsar.simulation.action;
 
 import org.pulsar.simulation.FieldMap;
-import org.pulsar.simulation.model.Coordinates;
 import org.pulsar.simulation.model.Entity;
 import org.pulsar.simulation.model.Ratio;
 import org.pulsar.simulation.model.Rock;
 
-import java.util.Random;
-
-public class RockGeneratorAction implements Action {
-
-    private final FieldMap map;
-    private final Ratio ratio;
-    private final Random random = new Random();
+public class RockGeneratorAction extends AbstractAction {
 
     private static final Entity ENTITY = new Rock();
     private static final Ratio DEFAULT_ROCK_RATIO = new Ratio(0.075, 0.125);
@@ -22,19 +15,11 @@ public class RockGeneratorAction implements Action {
     }
 
     public RockGeneratorAction(FieldMap map, Ratio ratio) {
-        this.map = map;
-        this.ratio = ratio;
+        super(map, ratio);
     }
 
     @Override
-    public void perform() {
-        int totalCells = map.getArea();
-        int totalEntities = (int) (totalCells * ratio.getInRange());
-
-        for (int i = 0; i < totalEntities; i++) {
-            // TODO - handle the exception
-            Coordinates randomCoordinates = map.getRandomAvailableCoordinates();
-            map.set(randomCoordinates, ENTITY);
-        }
+    public Entity createEntity() {
+        return ENTITY;
     }
 }
--- src/main/java/org/pulsar/simulation/action/TreeGeneratorAction.java
+++ src/main/java/org/pulsar/simulation/action/TreeGeneratorAction.java
@@ -1,18 +1,11 @@
 package org.pulsar.simulation.action;
 
 import org.pulsar.simulation.FieldMap;
-import org.pulsar.simulation.model.Coordinates;
 import org.pulsar.simulation.model.Entity;
 import org.pulsar.simulation.model.Ratio;
 import org.pulsar.simulation.model.Tree;
 
-import java.util.Random;
-
-public class TreeGeneratorAction implements Action {
-
-    private final FieldMap map;
-    private final Ratio ratio;
-    private final Random random = new Random(System.currentTimeMillis());
+public class TreeGeneratorAction extends AbstractAction {
 
     private static final Entity ENTITY = new Tree();
     private static final Ratio DEFAULT_TREE_RATIO = new Ratio(0.075, 0.125);
@@ -22,19 +15,11 @@ public class TreeGeneratorAction implements Action {
     }
 
     public TreeGeneratorAction(FieldMap map, Ratio ratio) {
-        this.map = map;
-        this.ratio = ratio;
+        super(map, ratio);
     }
 
     @Override
-    public void perform() {
-        int totalCells = map.getArea();
-        int totalEntities = (int) (totalCells * ratio.getInRange());
-
-        for (int i = 0; i < totalEntities; i++) {
-            // TODO - handle the exception
-            Coordinates randomCoordinates = map.getRandomAvailableCoordinates();
-            map.set(randomCoordinates, ENTITY);
-        }
+    public Entity createEntity() {
+        return ENTITY;
     }
 }
