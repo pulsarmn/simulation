505e31063c021f3f15958c723e3b01e323213be3
Кузичев Александр <pulsarmn@yandex.ru>
2025-08-28 16:19:19 +0300

    add: create a beta version of the `GrassGeneratorAction` class

---
+++ src/main/java/org/pulsar/simulation/action/GrassGeneratorAction.java
@@ -0,0 +1,25 @@
+package org.pulsar.simulation.action;
+
+import org.pulsar.simulation.FieldMap;
+import org.pulsar.simulation.model.Entity;
+import org.pulsar.simulation.model.Grass;
+import org.pulsar.simulation.model.Ratio;
+
+public class GrassGeneratorAction extends AbstractAction {
+
+    private static final Entity ENTITY = new Grass();
+    private static final Ratio DEFAULT_GRASS_RATIO = new Ratio(0.1, 0.175);
+
+    public GrassGeneratorAction(FieldMap map) {
+        this(map, DEFAULT_GRASS_RATIO);
+    }
+
+    public GrassGeneratorAction(FieldMap map, Ratio ratio) {
+        super(map, ratio);
+    }
+
+    @Override
+    public Entity createEntity() {
+        return ENTITY;
+    }
+}
