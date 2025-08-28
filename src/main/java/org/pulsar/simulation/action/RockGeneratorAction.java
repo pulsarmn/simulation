1176199145c9c9bef44e41720adb1582e6bedbb6
Кузичев Александр <pulsarmn@yandex.ru>
2025-08-28 15:47:15 +0300

    add: create a beta version of the `RockGenerationAction` class

---
+++ src/main/java/org/pulsar/simulation/action/RockGeneratorAction.java
@@ -0,0 +1,33 @@
+package org.pulsar.simulation.action;
+
+import org.pulsar.simulation.FieldMap;
+import org.pulsar.simulation.model.Coordinates;
+import org.pulsar.simulation.model.Entity;
+import org.pulsar.simulation.model.Ratio;
+import org.pulsar.simulation.model.Rock;
+
+import java.util.Random;
+
+public class RockGeneratorAction implements Action {
+
+    private final FieldMap map;
+    private final Ratio ratio;
+    private final Random random = new Random();
+
+    private static final Entity ENTITY = new Rock();
+    private static final Ratio DEFAULT_ROCK_RATIO = new Ratio(0.075, 0.125);
+
+    public RockGeneratorAction(FieldMap map) {
+        this(map, DEFAULT_ROCK_RATIO);
+    }
+
+    public RockGeneratorAction(FieldMap map, Ratio ratio) {
+        this.map = map;
+        this.ratio = ratio;
+    }
+
+    @Override
+    public void perform() {
+
+    }
+}
