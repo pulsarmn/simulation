b197da3dac4dc2e51b9570c8b11a578531e50426
Кузичев Александр <pulsarmn@yandex.ru>
2025-09-13 20:53:02 +0300

    add: the `MoveStrategy` field to the `Creature` class

--- src/main/java/org/pulsar/simulation/model/entity/Creature.java
+++ src/main/java/org/pulsar/simulation/model/entity/Creature.java
@@ -2,17 +2,22 @@ package org.pulsar.simulation.model.entity;
 
 import org.pulsar.simulation.model.Coordinates;
 import org.pulsar.simulation.model.Health;
+import org.pulsar.simulation.strategy.MoveStrategy;
 
 public abstract class Creature extends Entity {
 
     private final Health health;
+    private final MoveStrategy moveStrategy;
 
-    public Creature(String texture, Coordinates coordinates, Health health) {
+    public Creature(String texture, Coordinates coordinates, Health health, MoveStrategy moveStrategy) {
         super(texture, coordinates);
         this.health = health;
+        this.moveStrategy = moveStrategy;
     }
 
-    public abstract void makeMove();
+    public void makeMove() {
+        moveStrategy.move(this);
+    }
 
     public boolean isDead() {
         return health.isEmpty();
