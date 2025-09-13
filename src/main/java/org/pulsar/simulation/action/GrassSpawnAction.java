dcc5cc5b9d06c0e480f1cfd9708c52b4ddcc3e89
Кузичев Александр <pulsarmn@yandex.ru>
2025-09-13 20:55:05 +0300

    add: completaly redesign the `perform()` method in the `GrassSpawnAction` class

--- src/main/java/org/pulsar/simulation/action/GrassSpawnAction.java
+++ src/main/java/org/pulsar/simulation/action/GrassSpawnAction.java
@@ -5,8 +5,11 @@ import org.pulsar.simulation.model.Coordinates;
 import org.pulsar.simulation.model.Ratio;
 import org.pulsar.simulation.model.entity.Grass;
 
+import java.util.List;
+
 public class GrassSpawnAction extends AbstractSpawnAction<Grass> {
 
+    private static final int MIN_THRESHOLD = 2;
     private static final Ratio DEFAULT_RATIO = new Ratio(0.075, 0.125);
 
     public GrassSpawnAction(FieldMap fieldMap) {
@@ -17,11 +20,39 @@ public class GrassSpawnAction extends AbstractSpawnAction<Grass> {
         super(fieldMap, ratio);
     }
 
+    // Repeating code, I'm Lazy :(
     @Override
     public void perform() {
-        if (!getFieldMap().getAllByType(Grass.class).isEmpty()) {
+        List<Grass> grasses = getFieldMap().getAllByType(Grass.class);
+
+        if (grasses.isEmpty()) {
+            super.perform();
+            return;
+        }
+
+        int accessibleObjects = countAccessibleObjects(grasses);
+        if (accessibleObjects < MIN_THRESHOLD) {
             super.perform();
         }
+    }
+
+    private int countAccessibleObjects(List<Grass> grasses) {
+        int count = 0;
+        for (var grass : grasses) {
+            if (hasFreeNeighbour(grass.getCoordinates())) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean hasFreeNeighbour(Coordinates coordinates) {
+        for (var neighbour : getFieldMap().getNeighbours(coordinates)) {
+            if (getFieldMap().isCellFree(neighbour)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
